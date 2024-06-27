9debeedf9edde01ed1edec762a3ce5fc58d94aef
Maicon Alves <AD648ZZ@mmm.com>
2024-06-27 16:10:42 -0300

    Feat: General adjustments

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+    ssr: false,
+    nitro: {
+      prerender: {
+        crawlLinks: true
+      }
+    },
+    components: true,
+    app: {
+      buildAssetsDir: '/src/'
+    },
+    css: [
+        'primevue/resources/themes/aura-dark-green/theme.css',
+        'primevue/resources/primevue.css',
+        'primeicons/primeicons.css',
+        'primeflex/primeflex.css',
+    ],
+    modules: [
+        [
+          '@pinia/nuxt',
+          {
+            autoImports: [
+              'defineStore',
+              ['defineStore', 'definePiniaStore'],
+            ],
+          },
+        ],
+      ]
+})
